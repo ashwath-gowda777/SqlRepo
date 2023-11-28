9bee68f665a4908a5b198543b4b11d59b59eb7a8
Ashwath Gowda G <125112241+ashwath-gowda777@users.noreply.github.com>
2023-11-28 21:42:03 +0530

    This is the file for Assignment_question_2

--- src/Question_2.sql
+++ src/Question_2.sql
@@ -1,11 +1,11 @@
 /* 2.Create a table persons with first_name, last_name,city and age and add a constraint where age < 21
 
-1.Insert the  4 records with age less than 21 and atleast one record should have city value as “Mumbai”
+1.Insert the  4 records with age less than 21 and atleast one record should have city value as â€œMumbaiâ€
 2.Insert one more record which is greater than 21 and give the result
-3.Add one more constraint in the same table where city = “Mumbai”
-4.Insert one more record where city = “jaipur” and give the result
+3.Add one more constraint in the same table where city = â€œMumbaiâ€
+4.Insert one more record where city = â€œjaipurâ€ and give the result
 5.Drop both the Constraints.
-6.Create view where city = ‘Mumbai’ */
+6.Create view where city = â€˜Mumbaiâ€™ */
 
 --Create a table persons with first_name, last_name,city and age and add a constraint where age < 21
 create table persons(
@@ -15,7 +15,7 @@ city varchar(50),
 age int,
 constraint check_age CHECK(age<21));
 
---1.Insert the  4 records with age less than 21 and atleast one record should have city value as “Mumbai”
+--1.Insert the  4 records with age less than 21 and atleast one record should have city value as â€œMumbaiâ€
 Insert into persons(first_name,last_name,city,age)
 values
 ('Ashwath','Gowda','Bengaluru',17),
@@ -33,7 +33,7 @@ Insert into persons values('Radha','krishna','Mangalore',23);
 The INSERT statement conflicted with the CHECK constraint "check_age". The conflict occurred in database "ecommerce", table "dbo.persons", column 'age'.*/
 
 
---3.Add one more constraint in the same table where city = “Mumbai”
+--3.Add one more constraint in the same table where city = â€œMumbaiâ€
 ALTER TABLE persons 
 ADD CONSTRAINT chk_persons CHECK(city = 'Mumbai');
 --Result:You will get an error because conflict with existing data can result in errors if the data in the table doesn't meet the new constraint criteria.
@@ -41,7 +41,7 @@ ADD CONSTRAINT chk_persons CHECK(city = 'Mumbai');
 /*Msg 547, Level 16, State 0, Line 36
 The ALTER TABLE statement conflicted with the CHECK constraint "chk_persons". The conflict occurred in database "ecommerce", table "dbo.persons", column 'city'.*/
 
---4.Insert one more record where city = “jaipur” and give the result
+--4.Insert one more record where city = â€œjaipurâ€ and give the result
 Insert into persons values ('sravan','kumar','jaipur',16);
 
 select*from persons;
@@ -51,7 +51,7 @@ alter table persons
 drop constraint check_age;
 --In above as i couldn't add the constraint where i got an error - I droped the constraint
 
---6.Create view where city = ‘Mumbai’ */
+--6.Create view where city = â€˜Mumbaiâ€™ */
 CREATE VIEW Mumbai_View
 AS
 SELECT *
@@ -59,3 +59,4 @@ FROM persons
 WHERE city = 'Mumbai';
 
 select*from Mumbai_View;
+
